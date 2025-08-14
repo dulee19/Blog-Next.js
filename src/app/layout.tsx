4d0ea2aa473b63bc47eba0bfa245c920b4901ae8
dulee19 <adusan53@gmail.com>
2025-08-14 19:36:06 +0200

    eslint

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    // @ts-ignore
+
     <ClerkProvider>
       <html lang="en" suppressHydrationWarning>
         <body
